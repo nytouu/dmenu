c40f99381efdfd53789c1079d00b46d963a7cd18
nytouu <capitaine7312@gmail.com>
2021-06-06 16:19:35 +0200

    dmenu xyz

--- config.h
+++ config.h
@@ -4,16 +4,16 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Terminus:size=12"
+	"Roboto Mono:style=Medium:size=12"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = "run:";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#c9c9c9", "#181621" },
-	[SchemeSel] = { "#32203d", "#ccaaff" },
-	[SchemeOut] = { "#000000", "#c978ff" },
-	[SchemeSelHighlight] = { "#ffffff", "#ccaaff" },
-    [SchemeNormHighlight] = { "#ccaaff", "#181621" },
+	[SchemeNorm] = { "#c9c9c9", "#1a1b26" },
+	[SchemeSel] = { "#1a1b26", "#7aa2f7" },
+	[SchemeOut] = { "#000000", "#414868" },
+	[SchemeSelHighlight] = { "#ffffff", "#7aa2f7" },
+    [SchemeNormHighlight] = { "#7aa2f7", "#1a1b26" },
 	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
