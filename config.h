1d2dec0fb2ea68dbbdf7987342c5c72f67c69f5b
nytouu <capitaine7312@gmail.com>
2022-06-28 23:49:07 +0200

    config

--- config.h
+++ config.h
@@ -2,14 +2,14 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0x70;
+static const unsigned int alpha = 0xaf;
 static const unsigned int border_width = 0;
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:style=Medium:size=12"
+	"JetBrainsMono Nerd Font:style=Medium:size=12"
 };
-static const char *prompt      = "run:";      /* -p  option; prompt to the left of input field */
+static const char *prompt      = " ";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#c9c9c9", "#060608" },
