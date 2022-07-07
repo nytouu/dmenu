4260199e1e219af0ef6846647a779fd7b9c67c96
nytouu <capitaine7312@gmail.com>
2022-07-08 00:36:07 +0200

    config

--- config.h
+++ config.h
@@ -2,21 +2,22 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xaf;
+static const unsigned int alpha = OPAQUE;
+/* static const unsigned int alpha = 0xaf; */
 static const unsigned int border_width = 0;
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMono Nerd Font:style=Medium:size=12"
+	"SFMono Nerd Font:style=Regular:size=12"
 };
 static const char *prompt      = " ";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#c9c9c9", "#060608" },
-	[SchemeSel] = { "#060608", "#7aa2f7" },
-	[SchemeOut] = { "#000000", "#414868" },
+	[SchemeNorm] = { "#c9c9c9", "#1a1b26" },
+	[SchemeSel] = { "#1a1b26", "#7aa2f7" },
+	[SchemeOut] = { "#1a1b26", "#414868" },
 	[SchemeSelHighlight] = { "#ffffff", "#7aa2f7" },
-    [SchemeNormHighlight] = { "#7aa2f7", "#060608" },
+    [SchemeNormHighlight] = { "#7aa2f7", "#1a1b26" },
 	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
 
@@ -32,8 +33,8 @@ static const unsigned int alphas[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 28;
-static unsigned int min_lineheight = 28;
+static unsigned int lineheight = 32;
+static unsigned int min_lineheight = 32;
 
 /*
  * Characters not considered part of a word while deleting words
